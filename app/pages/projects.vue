572234dbcde86d65c1eebda3850b32e7c3f0d837
Mylan Mahieddine <mylanmahieddine@icloud.com>
2026-03-18 18:11:33 +0100

    modification description app Koda fautes

--- app/pages/projects.vue
+++ app/pages/projects.vue
@@ -144,7 +144,7 @@ const projects = [
   {
     title: 'Koda',
     category: 'perso' as Category,
-    description: 'KODA vous aide à rester concentré, gérer vos tâches efficacement et développer des habitudes de productivité durables grace a un tableau Kanban, un timer Pomodoro, statistiques détaillées et des objectifs quotidiens.',
+    description: 'KODA vous aide à rester concentré, gérer vos tâches efficacement et développer des habitudes de productivité durables grâce a un tableau Kanban, un timer Pomodoro, statistiques détaillées et des objectifs quotidiens.',
     tags: ['Vue.js', 'Dashboard', 'API', 'Vercel'],
     github: 'https://github.com/Mylan699/KODA',
     demo: 'https://koda-gray.vercel.app/',
